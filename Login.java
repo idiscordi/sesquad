d89ebf82150d89f01492e80d1896946e2da66c16
blakekeeling <blokcube@gmail.com>
2016-03-15 20:53:33 -0500

    basic login functions
    assumes dbhandler has static functions trylogin and newaccount
    todo: add login object to  comms

--- Login.java
+++ Login.java
@@ -1,1 +1,22 @@
 //handles login functionality, driven by servercomms and speaks to dbhandler
+java.lang.String
+class login{
+	private String password;
+	private String username;
+	
+	public login(String user, String pass){
+		password = pass;
+		username = user;
+	}
+	
+	public String trylogin(){
+		String str = dbhandler.trylogin(username, password);
+		return str;
+	}
+	
+	public String newaccount(){
+		if (username.contains("swear"){return "failed:contains swear"}; 
+		//future todo: check for explitives in username
+		return dbhandler.newaccount(username, password);
+	}
+}
