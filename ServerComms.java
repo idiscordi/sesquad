09d5e108ac9cc31a99f687903e045112166fb6be
idiscordi <jtvnwa@gmail.com>
2016-03-15 19:48:56 -0500

    Updated ServerComm.java
    cleaned skeleton up, tested functionality with testbench.  test procedures %java ServerComms %java ServCommTestBench

--- ServerComms.java
+++ ServerComms.java
@@ -2,9 +2,8 @@
 import java.io.*;
 import java.net.*;
 
-//turn me on to get console messages, off to kill verbose, could do this through CLI args later
-//e.g. java ServerComms would run without console printlns, java ServerComms -v would be verbose mode w/ outputs
-boolean debug = true;
+
+
 /*
  * This is currently a single connection w/ blocking wait
  * 
@@ -19,20 +18,21 @@ boolean debug = true;
 //making class
 class ServerComms
 {
+	//to kill server
+	static boolean shutdown = false;
+	//e.g. java ServerComms would run without console printlns, java ServerComms -v would be verbose mode w/ outputs
+	//turn me on to get console messages, off to kill verbose, could do this through CLI args later
+	static boolean debug = true;
+	
+	//call stopComms(true); and server will quit shut off comms
+	public void stopComms(boolean b)
+	{
+		shutdown = b;
+	}
+	
+	
 	public static void main (String args[])
 	{
-		//separate strings for clarity
-		String incoming;
-		String outbound;
-		//to kill server
-		boolean shutdown = false;
-		
-		//call stopComms(true); and server will quit shut off comms
-		public void stopComms(boolean b)
-		{
-			shutdown = b;
-		}
-		
 		
 		//TODO: Accept many communications, this is single blocking right now
 		try
@@ -48,21 +48,23 @@ class ServerComms
 				
 				//linking up w/ safe reader for instream and getting access to outstream
 				BufferedReader inputData = new BufferedReader(new InputStreamReader(client.getInputStream()));
-				DataOutputStream outputData = new DataOutputStream(client.getOutPutStream());
+				DataOutputStream outputData = new DataOutputStream(client.getOutputStream());
 				
 				//get some data
-				input = inputData.readline();
-				if(debug)
-					System.out.println("Received: " + input + "\n from client.");
+				input = inputData.readLine();
+				if(debug && input != null)
+					System.out.println("Received: " + input + " from client.");
 				
 				//TODO: parse message and pass to appropriate service if valid
 				
 				//TODO: wait on return message from service and reformat if necessary to send to client
 				
 				//respond with something
-				outputData.writeBytes("Server received your message as: " + input);
+				outputData.writeBytes("Server received your message as: " + input + "\n");
 				
 			}
+			listener.close();
 		}
+		catch(Exception e){ System.out.println(e.toString());}
 	}
 }
