66ca2f83612d13c3ababd3e1d7e062857a6dfb57
atlam105 <atlam@uark.edu>
2016-03-16 20:13:34 -0500

    Added files via upload

--- DBHandler.java
+++ DBHandler.java
@@ -1,1 +1,28 @@
-//interface for altering the db
+import java.sql.*;
+
+class DBHandler {
+	String dbusername;
+	String dbpassword;
+	boolean log = false;
+	
+	try {
+		
+		Connection conn = DriverManager.getConnection("jbdc:sqlserver://localhost:1433");
+		Statment stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT password FROM users WHERE username = " + username);
+	
+		while (rs.next()) {
+			dbusername = rs.getString("username");
+			dbpassword = rs.getString("password");
+		
+			if(dbusername.equals(username) && dbpassword.equals(password)) {
+				return "success:user and pass correct";
+				log = true;
+			}
+		}
+		conn.close();
+	} catch (Exception e) {
+		System.err.println("Exception Error");
+	}
+	return log;
+}
