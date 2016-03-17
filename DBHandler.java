eda3e764d3b127b56d8248263d4944fca5a528b7
atlam105 <atlam@uark.edu>
2016-03-16 20:28:08 -0500

    Fixed some issues

--- DBHandler.java
+++ DBHandler.java
@@ -3,26 +3,24 @@ import java.sql.*;
 class DBHandler {
 	String dbusername;
 	String dbpassword;
-	boolean log = false;
+
+	public static DBHandler {	
+		try {
+			Connection conn = DriverManager.getConnection("jbdc:sqlserver://localhost:1433");
+			Statment stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT password FROM users WHERE username = " + username);
 	
-	try {
+			while (rs.next()) {
+				dbusername = rs.getString("username");
+				dbpassword = rs.getString("password");
 		
-		Connection conn = DriverManager.getConnection("jbdc:sqlserver://localhost:1433");
-		Statment stmt = conn.createStatement();
-		ResultSet rs = stmt.executeQuery("SELECT password FROM users WHERE username = " + username);
-	
-		while (rs.next()) {
-			dbusername = rs.getString("username");
-			dbpassword = rs.getString("password");
-		
-			if(dbusername.equals(username) && dbpassword.equals(password)) {
-				return "success:user and pass correct";
-				log = true;
+				if(dbusername.equals(username) && dbpassword.equals(password)) {
+					conn.close();
+					return "success:user and pass correct";
+				}
 			}
+		} catch (Exception e) {
+			System.err.println("Exception Error");
 		}
-		conn.close();
-	} catch (Exception e) {
-		System.err.println("Exception Error");
-	}
-	return log;
+	}	
 }
