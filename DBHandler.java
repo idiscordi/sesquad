edb74732cda597209acb37ac72a80028b4f35068
atlam105 <atlam@uark.edu>
2016-03-16 21:06:39 -0500

    Added the 2nd method(unclear)

--- DBHandler.java
+++ DBHandler.java
@@ -3,6 +3,7 @@ import java.sql.*;
 class DBHandler {
 	String dbusername;
 	String dbpassword;
+	String dbemail;
 
 	public static DBHandler {	
 		try {
@@ -17,6 +18,26 @@ class DBHandler {
 				if(dbusername.equals(username) && dbpassword.equals(password)) {
 					conn.close();
 					return "success:user and pass correct";
+				}
+			}
+		} catch (Exception e) {
+			System.err.println("Exception Error");
+		}
+	}
+
+    public static DBHandler {
+		try {
+			Connection conn = DriverManager.getConnection("jbdc:sqlserver://localhost:1433");
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT email FROM users WHERE = " + username);
+			
+			while (rs.next()) {
+				dbusername = rs.getString("username");
+				dbemail = rs.getString("email");
+				
+				if(dbusername.equals(username) && dbemail.equals(email)) {
+					conn.close();
+					return "failed:Username and Email taken";
 				}
 			}
 		} catch (Exception e) {
