edef53eefcfe6f6abd8754c20b31639c35731dff
idiscordi <jtvnwa@gmail.com>
2016-03-17 21:22:45 -0500

    added console output to test
    basic test, console output, test harness

--- DBTest.java
+++ DBTest.java
@@ -10,11 +10,14 @@ public class DBTest {
 		String name;
 		String pass;
 		String email;
+		String response;
 		name = "test";
 		pass = "1234";
 		email = "no@thanks.com";
 		
-		DBHandler.DBLogin(name, pass);
+		System.out.println("Trying login with test:1234\n");
+		response = DBHandler.DBLogin(name, pass);
+		System.out.println("Handlers sends back: " response + "\n");
 	}
 
 }
