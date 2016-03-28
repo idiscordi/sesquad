332b5e3cc478e50c96de6c1a69a6429cfd92d55b
blakekeeling <blokcube@gmail.com>
2016-03-28 13:35:12 -0500

    parser: simplified logic
    streamlined logic for each command
    added template for adding new commands
    now drops instances of "::"

--- Parser.java
+++ Parser.java
@@ -6,35 +6,45 @@ public class Parser {
 	public static String Execute(String input, boolean debug){
 		try {
 			String output = "";
+			while(output.contains("::")){
+				output.replace("::", ":");
+			}
 			String[] commandarr = input.split(":");
 			//login command "login:username:password"
 			if (commandarr[0] == "login"){
-				if ((commandarr[1].length() != 0) && (commandarr[2].length() != 0)){
+				if (commandarr.length < 3){
+					output = "failed:" + commandarr[0] + ":command missing values";
+				} else {
 					Login loginInfo = new Login(commandarr[1],commandarr[2]);
 					String loginResults = loginInfo.trylogin();
-					if (debug)
-						System.out.println("login returned: " + loginResults);
 					output = loginResults;
-				} else {
-					output = "failed:" + commandarr[0] + ":command missing values";
 				}
 			}
-			//account creation commandarr "createaccount:username:password:email"
+			//account creation command "createaccount:username:password:email"
 			else if (commandarr[0] == "createaccount"){
-				if ((commandarr[1].length() != 0) && (commandarr[2].length() != 0) && (commandarr[3].length() != 0)){
+				if (commandarr.length < 4){
+					output = "failed:" + commandarr[0] + ":command missing values";
+				} else {
 					Login accountInfo = new Login(commandarr[1],commandarr[2]);
 					String accountResults = accountInfo.newaccount(commandarr[3]);
-					if (debug)
-						System.out.println("account creation returned: " + accountResults);
 					output = accountResults;
-				} else {
-					output = "failed:" + commandarr[0] + ":command missing values";
 				}
 			}
+			// template
+			/*
+			 * else if (commandarr[0] == "command"){
+				if (commandarr.length < 0){
+					output = "failed:" + commandarr[0] + ":command missing values";
+				} else {
+					output = result;
+					if (debug){System.out.println("command returned: " + result);}
+				}
+			}
+			*/
 			//default action
 			else{
 				if (debug)
-					System.out.println("input did not start with a valid command, started with: " + commandarr[0]);
+					System.out.println(commandarr[0] + " returned:" + output);
 			}
 			return output;
 		} catch (Exception e) {
