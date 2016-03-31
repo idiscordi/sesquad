590e0ab2709147abd8acac25ba5c7d59cf07252f
blakekeeling <blokcube@gmail.com>
2016-03-31 16:56:40 -0500

    added more commands
    added some iteraration 2 commands
    simplified logic again somehow

--- Parser.java
+++ Parser.java
@@ -11,45 +11,49 @@ public class Parser {
 			}
 			String[] commandarr = input.split(":");
 			//login command "login:username:password"
-			if (commandarr[0] == "login"){
-				if (commandarr.length < 3){
-					output = "failed:" + commandarr[0] + ":command missing values";
-				} else {
-					Login loginInfo = new Login(commandarr[1],commandarr[2]);
-					String loginResults = loginInfo.trylogin();
-					output = loginResults;
-				}
+			if ((commandarr[0] == "login") && (commandarr.length >= 3)){
+				Login loginInfo = new Login(commandarr[1],commandarr[2]);
+				String loginResults = loginInfo.trylogin();
+				output = loginResults;
 			}
 			//account creation command "createaccount:username:password:email"
-			else if (commandarr[0] == "createaccount"){
-				if (commandarr.length < 4){
-					output = "failed:" + commandarr[0] + ":command missing values";
-				} else {
-					Login accountInfo = new Login(commandarr[1],commandarr[2]);
-					String accountResults = accountInfo.newaccount(commandarr[3]);
-					output = accountResults;
-				}
+			else if ((commandarr[0] == "createaccount") && (commandarr.length >= 4)){
+				Login accountInfo = new Login(commandarr[1],commandarr[2]);
+				String accountResults = accountInfo.newaccount(commandarr[3]);
+				output = accountResults;
+			}
+			//join matchmaking command "findmatch:username"
+			else if ((commandarr[0] == "findmatch") && (commandarr.length >= 2)){
+			//	output = matchmaking.join(commandarr[1]);		
+			}
+			//exit matchmaking command "leavematch:username"
+			else if ((commandarr[0] == "leavematch") && (commandarr.length >= 2)){
+			//	output = matchmaking.leave(commandarr[1]);
+			}
+			//get game state "getstate:username:gameid"
+			else if ((commandarr[0] == "getstate") && (commandarr.length >= 3)){
+			//	output = matchmaking.getstate(commandarr[2], commandarr[1]);
+			}
+			//make move "gamemove:username:gameid:xcord,ycord:xcord,ycord: ..."
+			else if ((commandarr[0] == "gamemove") && (commandarr.length >= 4)){
+			//	output = class.makemove(commandarr);
 			}
 			// template
 			/*
-			 * else if (commandarr[0] == "command"){
-				if (commandarr.length < 0){
-					output = "failed:" + commandarr[0] + ":command missing values";
-				} else {
-					output = result;
-					if (debug){System.out.println("command returned: " + result);}
-				}
+		 	else if ((commandarr[0] == "command") && (commandarr.length >= 0)){
+			//	output = class.request(commandarr[x])
 			}
 			*/
 			//default action
 			else{
+				output = "error:" + commandarr[0] + ":is invalid or missing values";
+			}
 				if (debug)
 					System.out.println(commandarr[0] + " returned:" + output);
-			}
 			return output;
 		} catch (Exception e) {
 			System.out.print("parser error" + e.toString());
-			return "error:parser";
+			return "error:exception";
 		}
 	}
 
