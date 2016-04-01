ddac21f2446fae336b33bcd315c0688b3da34e1a
blakekeeling <blokcube@gmail.com>
2016-04-01 13:27:22 -0500

    added TODOs to game services
    just added more comments

--- GameServices.java
+++ GameServices.java
@@ -1,10 +1,14 @@
 //encapsulates matchmaking and the individual game services, driven by comms and relays back to client through comms
+//TODO make the run() sort the array before finding matches
+//TODO add a static method that removes a player from the matchmaking queue
+//TODO add a static method that will apply a move to a specified game
+//TODO add a static method that will return the complete game state of a specified game
 import java.util.*;
 public class GameServices implements Runnable {
 	
 	private static ArrayList<duck> line;
 	private static ArrayList<String> games;
-	//private static game[] games;
+	//private static game[] games; waiting on game object
 	
 	@Override
 	public void run() {
@@ -32,7 +36,7 @@ public class GameServices implements Runnable {
 			//logic for creating a game subject to change
 			if((prev.rating - curr.rating) <= Integer.max(prev.wait, curr.wait)){
 				synchronized (games){
-				//TODO create game instance
+				//TODO create game instance waiting on game object
 				//TODO notify players of game id
 				//TODO remove players from list
 				}
@@ -42,7 +46,7 @@ public class GameServices implements Runnable {
 
 	public static String join(String username){
 		try {
-			//TODO get user rating from DBhandler
+			//TODO get user rating from DBhandler waiting on DBhandler
 			duck player = new duck(username, "100");
 			if(player.rating < 0)
 				return "error:invalid rating:"+"0";
